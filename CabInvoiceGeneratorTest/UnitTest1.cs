ed39366fa19fad7668405e51c3c8d84b886ef24b
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-08-25 20:50:25 +0530

    Add project files.

---
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -0,0 +1,59 @@
+using CabInvoiceGenerator;
+
+namespace CabInvoiceGeneratorTest
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator = null;
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+            Assert.AreEqual(expected, fare);
+        }
+        [Test]
+        public void GivenMultipleRideShouldReturnInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 5) };
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 31.0);
+            Assert.AreEqual(summary, expectedSummary);
+        }
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFareforPremium()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            double distance = 25;
+            int time = 7;
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 389;
+            Assert.AreEqual(expected, fare);
+        }
+        [Test]
+        public void GivenMultipleRideShouldReturnInvoiceSummaryforPremium()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            Ride[] rides = { new Ride(10.0, 7), new Ride(15, 8) };//164,241
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 405);
+            Assert.AreEqual(summary, expectedSummary);
+        }
+        [Test]
+        public void GivenUserIdShouldReturnListOfRidesFromRepo()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 5), new Ride(3.0, 5) };//25,6,35
+            RideRepository rideRepository = new RideRepository();
+            rideRepository.AddRide("1", rides);
+            var rideArray = rideRepository.getRides("1");
+            InvoiceSummary summary = new InvoiceSummary(3, 66.0);
+            InvoiceSummary expected = invoiceGenerator.CalculateFare(rideArray);
+            Assert.AreEqual(expected, summary);
+        }
+    }
+}
